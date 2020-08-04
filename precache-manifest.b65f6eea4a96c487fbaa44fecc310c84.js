310346ed0fa017b453c8fd990d4e37c33000c2b5
Frolov Evgeny <fralik95@list.ru>
2020-08-04 11:02:36 +0300

    Updates

---
+++ precache-manifest.b65f6eea4a96c487fbaa44fecc310c84.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6d720606bcbb272bbe072bf9bce7ba",
+    "url": "/new-one/index.html"
+  },
+  {
+    "revision": "9d4e150df5e8790801c0",
+    "url": "/new-one/static/css/3.97cd49a0.chunk.css"
+  },
+  {
+    "revision": "8dca161f4ea017b9f6ec",
+    "url": "/new-one/static/css/main.eb5dc67b.chunk.css"
+  },
+  {
+    "revision": "63aa475b53564499b393",
+    "url": "/new-one/static/js/2.880a9ba1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/new-one/static/js/2.880a9ba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d4e150df5e8790801c0",
+    "url": "/new-one/static/js/3.2be035c9.chunk.js"
+  },
+  {
+    "revision": "8dca161f4ea017b9f6ec",
+    "url": "/new-one/static/js/main.00d5ebd2.chunk.js"
+  },
+  {
+    "revision": "4e300379d2509656a179",
+    "url": "/new-one/static/js/runtime-main.35f23651.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/new-one/static/media/preloader.26ef8bb4.gif"
+  },
+  {
+    "revision": "42b9ada56b782790c7dd0117fc201d9e",
+    "url": "/new-one/static/media/user2.42b9ada5.png"
+  }
+]);
